0281471856279deef4c8c76dfeb268fedbb8b33b
smhrd <smhrd@192.168.219.61>
2025-05-28 23:24:43 +0900

    첫번째 스프링 프로젝트 올리기

---
+++ MessageSystem/src/main/java/kr/smhrd/mapper/MySQL.sql
@@ -0,0 +1,9 @@
+create table fullStackmember(
+	email varchar(100) not null,
+	pw varchar(100) not null,
+	tel varchar(100) not null,
+	address varchar(100) not null,
+	primary key(email)
+);
+
+select * from fullStackMember;
